3a00e076ef64057edcb44376b21bd95c85030018
Daniel McRobb <dmcrobb@one.ai>
2024-01-02 04:22:06 -0500

    Start working on Response class.

---
+++ classes/include/DwmMcTallyResponse.hh
@@ -0,0 +1,85 @@
+//===========================================================================
+// @(#) $DwmPath$
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2024
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  \file DwmMcTallyResponse.hh
+//!  \author Daniel W. McRobb
+//!  \brief NOT YET DOCUMENTED
+//---------------------------------------------------------------------------
+
+#include <variant>
+
+#include "DwmStreamIOCapable.hh"
+#include "DwmMcTallyRequest.hh"
+#include "DwmMcTallyInstalledPackages.hh"
+#include "DwmMcTallyLoadAvg.hh"
+#include "DwmMcTallyUname.hh"
+
+namespace Dwm {
+
+  namespace McTally {
+
+    //------------------------------------------------------------------------
+    //!  Should encapsulate a response to a request.
+    //------------------------------------------------------------------------
+    class Response
+      : public StreamIOCapable
+    {
+    public:
+      Response();
+      Response(const Response & response) = default;
+      Response(Response && response) = default;
+      Response & operator = (const Response &) = default;
+      Response & operator = (Response &&) = default;
+      ~Response() = default;
+      
+      Response(const LoadAvg & avg);
+      Response(const Uname & uname);
+      Response(const InstalledPackages & pkgs);
+      
+      std::istream & Read(std::istream & is) override;
+      std::ostream & Write(std::ostream & os) const override;
+      
+    private:
+      Request                           _request;
+      std::variant<std::monostate,
+                   LoadAvg,
+                   Uname,
+                   InstalledPackages>   _responseData;
+    };
+    
+  }  // namespace McTally
+
+}  // namespace Dwm
